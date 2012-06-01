d78abaf23ee0d7808ce3d157a241e3b687eb441e
clux <analsandblaster@gmail.com>
2012-06-01 20:48:32 +0100

    a first preview version

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Functional JavaScript Programming
+The aim of this module (unlike many similar helper libraries) is not to replace/alias ES5 methods,
+nor is it to simply stuff a bunch of helpers on Array.prototype. Instead, it exports what has been shown by
+experience to be great additions to the ES5 reportoire as curried functions.
+
+Currently, only a preview version is available on npm, but docs, tests, and additions are coming.
+
+## Usage
+
+````javascript
+var f = require('functional');
+````
+
+Read the API (TODO)
+
+## Installation
+
+````bash
+$ npm install functional
+````
+
+## License
+MIT-Licensed. See LICENSE file for details.
--- functional.js
+++ functional.js
@@ -1,28 +1,51 @@
 var slice = Array.prototype.slice;
+var f = {};
+
+// common
+var id = f.id = function (x) {
+  return x;
+};
+
+var noop = f.noop = function () {
+};
+
+var constant = f.constant = function (val) {
+  return function () {
+    return val;
+  };
+};
 
 // curried helper functions for common operations
-exports.plus = function (x) {
+f.plus = function (x) {
   return function (y) {
     return y + x;
   };
 };
 
-exports.times = function (x) {
+f.times = function (x) {
   return function (y) {
     return y * x;
   };
 };
 
-exports.append = function (xs) {
+f.append = function (xs) {
   return function (ys) {
     return ys.concat(xs);
+  };
+};
+
+f.compose = function (f) {
+  return function (g) {
+    return function (x) {
+      return f(g(x));
+    };
   };
 };
 
 
 
 // fold - Array::reduce with array curried
-var fold = exports.fold = function (f, initial) {
+var fold = f.fold = function (f, initial) {
   return function (xs) {
     return xs.reduce(f, initial);
   };
@@ -57,42 +80,52 @@ var chain = function (f, g) {
 
 
 // using fold with binary operators to give lifted functions
-exports.sum = fold(add, 0);
-exports.product = fold(multiply, 1);
-exports.flatten = fold(concat, []);
-exports.and = fold(both, true);
-exports.or = fold(either, false);
+// any of these (i.e. they act on a list) can be remembered by "take the " fnName
+// wheras the variadic counterpart has the name of the associative operator
+f.sum = fold(add, 0);
+f.product = fold(multiply, 1);
+f.flat = fold(concat, []);
+f.composition = fold(chain, id);
 
+// we could export these
+//f.and = fold(both, true);
+//f.or = fold(either, false);
 
-
-// lifts and unlifts:
-
-
-// take a function operating on an array and turn it into a multi-parameter function, inverse of lift
-var unlift = exports.unlift = function (f, context) {
-  return function () {
-    var args = slice.call(arguments, 0);
-    return f.apply(context, [args]);
+// but prefer faster ES5 methods
+// best semantically; "we take the logical" and/or of list
+f.and = function (f) {
+  return function (xs) {
+    return xs.every(f);
   };
 };
 
+f.or = function (f) {
+  return function (xs) {
+    return xs.some(f);
+  };
+};
+
+// lifts and unlifts:
+// because some functions are already variadic
+
 // lift a multi param function f into a single array fn, inverse of unlift
-var lift = exports.lift = function (f, context) {
+var lift = f.lift = function (f, context) {
   return function (xs) {
     return f.apply(context, xs);
   };
 };
 
 // examples
+// follows the "take the" fnName of list semantic
 // max/min already have unlifted variadic counterparts so they can simply be lifted
-var maximum = lift(Math.max, Math);
-var minimum = lift(Math.min, Math);
+var maximum = f.maximum = lift(Math.max, Math);
+var minimum = f.maximum = lift(Math.min, Math);
 
 
 // (new ClassInst) can't be lifted - has to be one manually
 // maybe not want to put this in here, after all Object.create exists
 // and classes are a different thing for people to disagree over
-exports.construct = function (Ctor, args) {
+f.construct = function (Ctor, args) {
   var F = function () {
     Ctor.apply(this, args);
   };
@@ -100,11 +133,24 @@ exports.construct = function (Ctor, args) {
   return new F();
 };
 
-exports.addN = unlift(exports.sum);
-exports.multiplyN = unlift(exports.product);
-exports.concatN = unlift(exports.flatten);
-exports.andN = unlift(exports.and);
-exports.orN = unlift(exports.or);
+
+// take a function operating on an array and turn it into a multi-parameter function, inverse of lift
+// because argument based functions are semantic tand is most sensible with zipWith
+var unlift = f.unlift = function (f, context) {
+  return function () {
+    var args = slice.call(arguments, 0);
+    return f.apply(context, [args]);
+  };
+};
+
+// variadic version of lift functions
+// named after the operation, i.e. "we " fnName (together) "arg1, arg2, ..."
+f.add = unlift(f.sum);
+f.multiply = unlift(f.product);
+f.concat = unlift(f.flatten);
+f.and = unlift(f.all);
+f.or = unlift(f.any);
+f.compose = unlift(f.composition);
 
 
 // getters/setters
@@ -112,7 +158,7 @@ exports.orN = unlift(exports.or);
 // simple proprety accessor
 // can also be used for array number accessor
 // get :: Int/String -> Property
-exports.get = function (prop) {
+f.get = function (prop) {
   return function (el) {
     return el[prop];
   };
@@ -121,7 +167,7 @@ exports.get = function (prop) {
 // property get map -- equivalent to _.pluck or map(get('prop'))
 // zip(pmap('length'), [[1,3,2],[2,1]], [[1],[2]], [[2,3],[2,1]]) -> [[3,1,2], [2,1,2]]
 // pmap :: String -> [a] -> [b] -- both curried and as a two param fn
-var pmap = exports.pmap = function (propName, ary) {
+var pmap = f.pmap = function (propName, ary) {
   var fn = function (xs) {
     var result = [];
     for (var i = 0; i < xs.length; i += 1) {
@@ -132,7 +178,7 @@ var pmap = exports.pmap = function (propName, ary) {
   return (ary == null) ? fn : fn(ary);
 };
 
-exports.set = function (propName) {
+f.set = function (propName) {
   return function (el, value) {
     el[propName] = value;
     return el;
@@ -141,7 +187,7 @@ exports.set = function (propName) {
 
 // modify a list of objects by setting propName on all objects to valFn(currObj)
 // property set map -- equivalent to map(set('prop'))
-var mmap = exports.mmap = function (propName, valFn) {
+var mmap = f.mmap = function (propName, valFn) {
   return function (xs) {
     for (var i = 0; i < xs.length; i += 1) {
       xs[i][propName] = valFn(xs[i]);
@@ -156,14 +202,8 @@ var mmap = exports.mmap = function (propName, valFn) {
 
 // general map/zip helpers
 
-var constant = exports.constant = function (val) {
-  return function () {
-    return val;
-  };
-};
-
 // equivalent to _.range
-exports.range = function (start, stop, step) {
+f.range = function (start, stop, step) {
   if (arguments.length <= 1) {
     stop = start || 0;
     start = 0;
@@ -182,7 +222,7 @@ exports.range = function (start, stop, step) {
   return range;
 };
 
-exports.iterate = function (f, times) {
+f.iterate = function (f, times) {
   return function (x) {
     var result = [x];
     for (var i = 1; i < times; i += 1) {
@@ -200,7 +240,7 @@ exports.iterate = function (f, times) {
 // zipper function must have the same number of arguments as there are lists
 // but beyond that, it's very dynamic
 // zipWith(function(x,y,z){return x+y+z;}, [1,3,2], [21,1], [2,3]) -> [24,7]
-var zipWith = function () {
+f.zipWith = function () {
   var fn = arguments[0]
     , args = slice.call(arguments, 1)
     , numLists = args.length
@@ -218,8 +258,8 @@ var zipWith = function () {
 };
 
 // zip, zip3, zip4.. all in one!
-var zip = function () {
-  return zipWith.apply(null, [null].concat(slice.call(arguments,0)));
+f.zip = function () {
+  return f.zipWith.apply(null, [null].concat(slice.call(arguments, 0)));
 };
 
 
@@ -228,7 +268,7 @@ var zip = function () {
 // sort helper
 // put in property names (in order), you want to order by
 // then pass the resulting function to ary.sort()
-var comparing = function () {
+f.comparing = function () {
   var pargs = slice.call(arguments, 0);
   return function (x, y) {
     for (var i = 0; i < pargs.length; i += 1) {
@@ -242,66 +282,53 @@ var comparing = function () {
 
 
 // functional comparison helpers
-exports.gt = function (a) {
+f.gt = function (a) {
   return function (b) {
     return b > a;
   };
 };
 
-exports.lt = function (a) {
+f.lt = function (a) {
   return function (b) {
     return b < a;
   };
 };
 
-exports.eq = function (a) {
+f.eq = function (a) {
   return function (b) {
     return b === a;
   };
 };
 
 // weak eq
-exports.weq = function (a) {
+f.weq = function (a) {
   return function (b) {
     return b == a;
   };
 };
 
-exports.gte = function (a) {
+f.gte = function (a) {
   return function (b) {
     return b >= a;
   };
 };
 
-exports.lte = function (a) {
+f.lte = function (a) {
   return function (b) {
     return b >= a;
   };
 };
+
 // can do stuff like
 // [1,4,2,5,2,3].filter(gt(3)); -> [4,5]
 
-// any / all helpers
-// if uncurried use preferred, use ES5 methods: Array::some, Array::every
-exports.any = function (f) {
-  return function (xs) {
-    return xs.some(f);
-  };
-};
-
-exports.all = function (f) {
-  return function (xs) {
-    return xs.every(f);
-  };
-};
-
-exports.elem = function (ary) {
+f.elem = function (ary) {
   return function (x) {
     return ary.indexOf(x) >= 0;
   };
 };
 
-exports.notElem = function (ary) {
+f.notElem = function (ary) {
   return function (x) {
     return ary.indexOf(x) < 0;
   };
@@ -313,37 +340,101 @@ exports.notElem = function (ary) {
 // [1,2,3,4,3].filter(notElem[1,3]) -> [2,4]
 
 // nub, nubBy, intersect, intersectBy?
+f.nub = function (ary) {
+  var result = [];
+  for (var i = 0; i < ary.length; i += 1) {
+    if (ary.indexOf(ary[i], i+1) < 0) {
+      result.push(ary[i]);
+    }
+  }
+  return result;
+};
 
 
-//exports.either null guard a function, else produce a warning?
-
-
-var curry = function (fn) {
+f.curry = function (fn) {
   var curried = slice.call(arguments, 1);
   return function () {
     var args = curried.concat(slice.call(arguments, 0));
     return fn.apply(this, args);
+  };
+};
+
+// like curry, but curries the last arguments, and creates a function expecting the first
+f.rcurry = function (fn) {
+  var curried = slice.call(arguments, 1);
+  return function () {
+    var args = slice.call(arguments, 0).concat(curried);
+    return fn.apply(this, args);
+  };
+};
+
+// sequence(f1, f2, f3..., fn)(args...) == fn(...(f3(f2(f1(args...)))))
+// f.sequence(f.plus(2), f.plus(3), f.times(2))(2) -> 14
+f.sequence = function () {
+  var fns = slice.call(arguments, 0)
+    , argLen = fns.length;
+  return function () {
+    var result = slice.call(arguments, 0);
+    for (var i = 0; i < argLen; i += 1) {
+      result = [fns[i].apply(this, result)];
+    }
+    return result[0];
+  };
 };
 
 
 // guard a function by a condition function
 // returns a function that will only apply f(x) if cond(x) is true
-exports.guard = function (fn, cond) {
+f.guard = function (fn, cond) {
   return function (x) {
     return (cond(x)) ? fn(x) : null;
   };
 };
 
-// var cpuSafeFibonacci = guard(fibonacci, lt(100));
+// var guardedFibonacci = f.guard(fibonacci, lt(100));
 
-exports.pow = function (exponent) {
+// f.either null guard a function, else return errorFn result
+// if errorFn is a logger, then curry it with the required message
+f.either = function (guardedFn, errorFn) {
+  return function (x) {
+    var result = guardedFn(x);
+    return (result === null) ? errorFn() : result;
+  };
+};
+
+// var errorMsg;
+// var cpuSafeFibonacci = f.either(guardedFibonacci, f.constant(errorMsg));
+// or
+// var cpuSafeFibonaci = f.either(guardedFibonacci, f.curry(console.log, errorMsg))
+
+
+
+f.pow = function (exponent) {
   return function (x) {
     return Math.pow(x, exponent);
   };
 };
 
-exports.logBase = function (base) {
+f.logBase = function (base) {
   return function (x) {
     return Math.log(x) / Math.log(base);
   };
 };
+
+
+// debug function, wrap it in a function reporting its scope and arguments
+// particularly useful when combined with f.iterate
+f.trace = function (fn, fnName) {
+  var log = (console) ? console.log : noop
+    , global = (function () { return this; }())
+    , name = fn.name || fnName || "fn";
+
+  return function () {
+    var result = fn.apply(this, arguments);
+    var scope = (this === global) ? "global" : this;
+    log('[', name + '.apply(', scope, ',', slice.call(arguments, 0), ') -> ', result, ']');
+    return result;
+  };
+};
+
+module.exports = f;
